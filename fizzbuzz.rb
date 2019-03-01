f998ac5dc600d2d4a7c6a46611cd5dec688d7a67
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-01 16:06:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  fizzbuzz_int = fizzbuzz(int)
-  if int % 3 == 0 
-    return "Fizz"
+
+  if int % 5 == 0 && int % 3 == 0
+    return "FizzBuzz"
   elsif int % 5 == 0 
     return "Buzz"
-  elsif int % 5 == 0 && int % 3 == 0 
-  return "FizzBuzz"
+  elsif  int % 3 == 0 
+  return "Fizz"
 else 
   nil 
  end
